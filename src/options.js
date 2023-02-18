078012a6cfc40495eae5482f780351b4fb5bbca3
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2023-02-19 01:19:59 +0200

    new db data

--- src/options.js
+++ src/options.js
@@ -1,5 +1,236 @@
 export const options = [
-    { value: 'Option 1', label: 'Option 1' },
-    { value: 'Option 2', label: 'Option 2' },
-    { value: 'Option 3', label: 'Option 3' },
-  ];
+  {
+    id: "544200970",
+    billDate: "2023-01-20T08:00:00.000+00:00",
+    paymentDueDate: "2023-02-03T08:00:00.000+00:00",
+    nextBillDate: "2023-02-20T08:00:00.000+00:00",
+    billNo: "VDF544200970",
+    amountDue: {
+      value: 10,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-12-20T08:00:00.000+00:00",
+      endDateTime: "2023-01-19T08:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnlineBalance",
+  },
+  {
+    id: "538752189",
+    billDate: "2022-12-20T08:00:00.000+00:00",
+    paymentDueDate: "2023-01-03T08:00:00.000+00:00",
+    nextBillDate: "2023-01-20T08:00:00.000+00:00",
+    billNo: "VDF538752189",
+    amountDue: {
+      value: 20,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-11-20T08:00:00.000+00:00",
+      endDateTime: "2022-12-19T08:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnline",
+  },
+  {
+    id: "532927595",
+    billDate: "2022-11-20T08:00:00.000+00:00",
+    paymentDueDate: "2022-12-04T08:00:00.000+00:00",
+    nextBillDate: "2022-12-20T08:00:00.000+00:00",
+    billNo: "VDF532927595",
+    amountDue: {
+      value: 120,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-10-20T07:00:00.000+00:00",
+      endDateTime: "2022-11-19T08:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnline",
+  },
+  {
+    id: "527207838",
+    billDate: "2022-10-20T07:00:00.000+00:00",
+    paymentDueDate: "2022-11-03T08:00:00.000+00:00",
+    nextBillDate: "2022-11-20T08:00:00.000+00:00",
+    billNo: "VDF527207838",
+    amountDue: {
+      value: 210,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-09-20T07:00:00.000+00:00",
+      endDateTime: "2022-10-19T07:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnline",
+  },
+  {
+    id: "521756576",
+    billDate: "2022-09-20T07:00:00.000+00:00",
+    paymentDueDate: "2022-10-04T07:00:00.000+00:00",
+    nextBillDate: "2022-10-20T07:00:00.000+00:00",
+    billNo: "VDF521756576",
+    amountDue: {
+      value: 310,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-08-20T07:00:00.000+00:00",
+      endDateTime: "2022-09-19T07:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnline",
+  },
+  {
+    id: "515999990",
+    billDate: "2022-08-20T07:00:00.000+00:00",
+    paymentDueDate: "2022-09-03T07:00:00.000+00:00",
+    nextBillDate: "2022-09-20T07:00:00.000+00:00",
+    billNo: "VDF515999990",
+    amountDue: {
+      value: 410,
+      unit: "lei",
+    },
+    billingPeriod: {
+      startDateTime: "2022-07-20T07:00:00.000+00:00",
+      endDateTime: "2022-08-19T07:00:00.000+00:00",
+    },
+    financialAccount: {
+      accountBalance: {
+        amount: {
+          value: 0,
+          unit: "lei",
+        },
+        validFor: {
+          startDateTime: "2023-02-18T23:11:50.818+00:00",
+          endDateTime: null,
+        },
+      },
+      id: "238446101",
+    },
+    billDocument: [
+      {
+        attachmentType: "PDF_1P",
+        mimeType: "application/pdf",
+      },
+      {
+        attachmentType: "PDF_2P",
+        mimeType: "application/pdf",
+      },
+    ],
+    "@type": "CustomerBillOnline",
+  },
+];
