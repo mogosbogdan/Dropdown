534d140b84b93c06709babe43fcd7968324d9da1
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2023-02-19 01:20:12 +0200

    implementing new db data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
 import { options } from "./options";
+import dateFormat from "dateformat";
 import "./App.css";
+import { translatedBillMonth } from "./TranslatedBillMonth";
 
 const DropdownMenu = ({ value, options, onChange }) => {
   return (
-    <select value={value} onChange={onChange}>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
+    <select value={value} onChange={onChange} className="dropdown">
+      {options.map((option) => {
+        const billMonth = dateFormat(option.billDate, "mmmm");
+        return (
+          <option key={option.billDate} value={option.billDate}>
+            Factura {translatedBillMonth(billMonth)}
+          </option>
+        );
+      })}
     </select>
   );
 };
-
 const DisplayedOption = ({ option }) => {
-  return <div style={{ marginTop: 20 }}>Selected option: {option.label}</div>;
+  const billMonth = dateFormat(option.billDate, "mmmm");
+
+  return (
+    <div className="invoiceDetails">
+      <div>Factura {translatedBillMonth(billMonth)}</div>
+      <div>
+        {"Due: " + option.amountDue.value + " " + option.amountDue.unit}
+      </div>
+      <div>{"Bill: " + option.billNo}</div>
+      <div>{"ID: " + option.id}</div>
+    </div>
+  );
 };
 
 const SelectDropdown = () => {
@@ -23,8 +38,9 @@ const SelectDropdown = () => {
 
   const handleSelectChange = (event) => {
     const value = event.target.value;
-    const option = options.find((option) => option.value === value);
+    const option = options.find((option) => option.billDate === value);
     setSelectedOption(option);
+    console.log(selectedOption);
   };
 
   return (
